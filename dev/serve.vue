729e889679e4b3cc7bca9ec12a9103cc695576c6
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2021-05-06 17:18:57 +0200

    Test push

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { OnVueLibrarySample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  OnVueLibrarySample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <on-vue-library-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as OnVueLibrarySample } from './on-vue-library-sample.vue';
